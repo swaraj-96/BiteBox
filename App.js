0120a4d759e5139e88cb54aa2a2a0ff333451ef4
SWARAJ PRADHAN <pswaraj96@gmail.com>
2023-11-06 21:46:57 +0530

    episode2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import  ReactDOM  from "react-dom/client";
+
 /*  <div id = "parent">
            <div id = "child1">
                <h1>I'm h1 tag</h1>
@@ -12,7 +15,7 @@
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h1", {}, "I'm h1 tagg"),
     React.createElement("h2", {}, "I'm h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
