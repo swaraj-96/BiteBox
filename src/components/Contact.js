3b3fafe3ac5aa2686917ce80350e6a6717bc77a1
SWARAJ PRADHAN <pswaraj96@gmail.com>
2023-12-21 22:41:02 +0530

    contact page updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,37 @@
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";
+
 const Contact = () => {
   return (
-    <div>
-      <h1 className="font bold text-3xl">Contact</h1>
+    <div className="max-w-[768px] w-[90%] mx-auto py-16  min-h-[80vh] items-center">
+      <div className="bg-white py-8 rounded-br-2xl shadow-2xl px-8 text-base">
+        <p className="text-xs text-gray-500">Hello,</p>
+        <h1 className="text-2xl font-bold">I'm Swaraj Pradhan</h1>
+        <p className="text-indigo-400 font-semibold">Frontend Developer</p>
+        <div className="my-4">
+          <h2 className="font-bold text-lg text-btnTheme">Contact Me</h2>
+          <div className="flex mt-2 gap-2 items-center">
+            <IoIosMail className="w-8 h-8 text-gray-700" />
+            <p className="text-gray-500 text-base">pswaraj96@gmail.com</p>
+          </div>
+
+          <div className="flex gap-6 items-center my-4">
+            <a href="https://github.com/swaraj-96">
+              <FaGithub className="w-8 h-8 text-gray-700 transition ease-in-out   hover:scale-110 duration-300 hover:-translate-y-1 hover:text-red-800" />
+            </a>
+            <a href="https://www.linkedin.com/in/swaraj-pradhan-92b347173">
+              <FaLinkedin className="w-8 h-8 text-gray-700 transition ease-in-out  hover:scale-110 duration-300 hover:-translate-y-1 hover:text-red-800" />
+            </a>
+            <a href="https://twitter.com/swaraj_sj96">
+              <FaXTwitter className="w-8 h-8 text-gray-700 transition ease-in-out   hover:scale-110 duration-300 hover:-translate-y-1 hover:text-red-800" />
+            </a>
+            <a href="https://github.com/swaraj-96">
+              <FaInstagram className="w-8 h-8 text-gray-700 transition ease-in-out   hover:scale-110 duration-300 hover:-translate-y-1 hover:text-red-800" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
