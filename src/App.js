cca90146f06c0059273afd5e0d94c84bf675a881
SWARAJ PRADHAN <pswaraj96@gmail.com>
2023-12-28 01:01:10 +0530

    checking all of them

--- src/App.js
+++ src/App.js
@@ -6,44 +6,22 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
-import {
-  createBrowserRouter,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
-import { Provider} from "react-redux";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import appStore from "../utils/appStore";
 import Cart from "./components/Cart";
 import Footer from "./components/Footer";
-import { ThemeProvider } from "./contexts/ThemeContext";
 
 //this is for home page/parent route layout
 const AppLayout = () => {
-
-  const [themeMode, setThemeMode] = useState("light");
-  const darkMode = () => {
-     setThemeMode("dark");
-   };
-   const lightMode = () => {
-    setThemeMode("light");
-  };
-
-  useEffect(() => {
-    document.querySelector("html").classList.remove("dark", "light");
-    document.querySelector("html").classList.add(themeMode);
-  }, [themeMode]);
-
-
   return (
     // provider is used to wrap our whole app and connect it to our central store with props value as our store name.
     <Provider store={appStore}>
-      <ThemeProvider value={{themeMode, darkMode, lightMode}}>
       <div className="app flex flex-col min-h-screen bg-bgTheme">
         <Header />
         <Outlet />
         <Footer />
       </div>
-      </ThemeProvider>
     </Provider>
   );
 };
