64c1e122240fe43c3c0d3cf657b939654eb553e1
SWARAJ PRADHAN <pswaraj96@gmail.com>
2023-12-27 22:25:50 +0530

    minor bug check

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,7 +6,7 @@ import img1 from '../../images/Logo.png'
 const Logo = () => {
   return (
     <Link to="/" >
-      <img className="md:w-20 w-16  p-2 items-center transition ease-in-out delay-150  hover:scale-110 duration-300 " src= {img1} />
+      <img className="md:w-20 w-16  p-2 items-center transition ease-in-out delay-150  hover:scale-110 duration-300 " src= {LOGO_URL} />
     </Link>
   );
 };
