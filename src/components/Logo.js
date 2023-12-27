18a36956eb14340dc7baed62eb6e3226d7fe3d8c
SWARAJ PRADHAN <pswaraj96@gmail.com>
2023-12-28 00:58:20 +0530

    Revert "modified all"
    This reverts commit bce419635a859ba9a9b86c5445204a7dc569a5fc.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { LOGO_URL } from "../../utils/constants";
-
+import img1 from '../../images/Logo.png'
 
 const Logo = () => {
   return (
     <Link to="/" >
-      <img className="md:w-20 w-16  p-2 items-center transition ease-in-out delay-150  hover:scale-110 duration-300 " src= {LOGO_URL} />
+      <img className="md:w-20 w-16  p-2 items-center transition ease-in-out delay-150  hover:scale-110 duration-300 " src= {img1} />
     </Link>
   );
 };
