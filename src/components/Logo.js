c2c1b74238b32f615b8a69fe59d1fe9a5b40e544
SWARAJ PRADHAN <pswaraj96@gmail.com>
2023-12-28 11:44:10 +0530

    logo checking

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { LOGO_URL } from "../../utils/constants";
+import logo from '../../images/Logo.png'
 
 
 const Logo = () => {
   return (
     <div className="item-center mr-0">
        <Link to="/" >
-      <img className="md:w-20 w-[50px] p-2 md:p-2 transition ease-in-out delay-150  hover:scale-110 duration-300 " src= {LOGO_URL} />
+      <img className="md:w-20 w-[50px] p-2 md:p-2 transition ease-in-out delay-150  hover:scale-110 duration-300 " src= {logo} />
     </Link>
     </div>
     
