701dc8d84e179297f1d0ab83596be337460ea590
SWARAJ PRADHAN <pswaraj96@gmail.com>
2023-12-28 11:31:51 +0530

    ui checking

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,9 +5,12 @@ import { LOGO_URL } from "../../utils/constants";
 
 const Logo = () => {
   return (
-    <Link to="/" >
-      <img className="md:w-20 w-16  p-2 items-center transition ease-in-out delay-150  hover:scale-110 duration-300 " src= {LOGO_URL} />
+    <div className="item-center mr-0">
+       <Link to="/" >
+      <img className="md:w-20 w-12  py-2 pr-2 md:p-2 transition ease-in-out delay-150  hover:scale-110 duration-300 " src= {LOGO_URL} />
     </Link>
+    </div>
+    
   );
 };
 
