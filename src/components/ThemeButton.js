d9ad7d8cca9da1adc7d38f3ff2b78fa07caa76c7
SWARAJ PRADHAN <pswaraj96@gmail.com>
2023-12-28 11:18:02 +0530

    theme button responsiveness check

--- src/components/ThemeButton.js
+++ src/components/ThemeButton.js
@@ -17,23 +17,23 @@ const ThemeButton = () => {
    
   return (
     <>
-    <label className='themeSwitcherTwo relative inline-flex cursor-pointer select-none items-center'>
+    <label className='themeSwitcherTwo relative  cursor-pointer select-none items-center'>
         <input
           type='checkbox'
           checked={darkMode}
           onChange={() => {
             setDarkMode(!darkMode)
           }}
-          className='sr-only'
+          className='sr-only w-6'
         />
         <span
-          className={`slider mx-4 flex h-8 w-[60px] items-center rounded-full p-1 duration-200 ${
+          className={`slider mx-1 flex md:h-8 md:w-[60px] h-6 w-[40px] items-center rounded-full p-1 duration-200 ${
             darkMode ? 'bg-[#374151]' : 'bg-[#CCCCCE]'
           }`}
         >
           <span
-            className={`dot h-6 w-6 rounded-full bg-white duration-200 ${
-              darkMode? 'translate-x-[28px]' : ''
+            className={`dot md:h-6 md:w-6 h-3 w-3 rounded-full bg-white  duration-200 ${
+              darkMode? 'md:translate-x-[28px] translate-x-[20px]' : ''
             }`}
           ></span>
         </span>
