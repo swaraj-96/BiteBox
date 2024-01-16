3a558ce53a6aefb9afd245fba1ec379f1336db92
SWARAJ PRADHAN <pswaraj96@gmail.com>
2024-01-16 20:41:20 +0530

    api updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,7 +19,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://bitebox-swaraj.onrender.com/api/restaurants?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://bitebox-swaraj.onrender.com/api/restaurants?lat=20.3532772&lng=85.8265977&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(json);
